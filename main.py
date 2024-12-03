335abd84ed8cb22affc94201ee6668158cd9887f
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2024-12-03 23:20:50 +0700

    chore: return temp on predict

--- main.py
+++ main.py
@@ -7,15 +7,22 @@ import uvicorn
 
 app = FastAPI()
 
+
 @app.get("/")
 async def hello_world():
     return {"Hello": "World"}
 
+
 @app.post("/predict")
 async def predict(request: Request):
     body = await request.json()
-    predict = utils.recommender(body['user_id'], body['skillset'])
+    # predict = utils.recommender(body['user_id'], body['skillset'])
+
+    # TODO: ADJUST WITH REAL DATA
+    predict = [1, 2, 3, 4, 5]
+
     return predict
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
